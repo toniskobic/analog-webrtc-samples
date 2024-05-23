d27f3cb7152227380688e7279e91b71bcd3f5028
Toni Škobić <toni.skobic@gmail.com>
2024-05-23 19:11:38 +0200

    feat: migrate to angular 18 and remove zone.js

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection as provideZonelessChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideFileRouter } from '@analogjs/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZonelessChangeDetection(),
     provideFileRouter(),
     provideHttpClient(withFetch()),
     provideClientHydration(),
